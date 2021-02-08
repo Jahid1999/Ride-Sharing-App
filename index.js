0cd2af0c846907abb9517c6993bafc6b969ffe0f
Jahid1999 <bsse1030@iit.du.ac.bd>
2021-02-09 00:34:43 +0600

    connected with db, created table

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { POINT_CONVERSION_COMPRESSED } = require('constants');
 const express = require('express');
 const mysql = require('mysql');
 
@@ -7,6 +8,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'iit123',
+    database: 'ride_share'
 });
 
 //Connect 
@@ -24,6 +26,7 @@ app.get('/', (req, res) => {
     res.send('Welcome Ride Share');
 });
 
+//Create database
 app.get('/createDB', (req, res) => {
     let query = 'CREATE DATABASE ride_share';
     db.query(query, (err, result) => {
@@ -31,6 +34,45 @@ app.get('/createDB', (req, res) => {
             throw err;
         console.log(result);
         res.send('Database Created');
+    })
+});
+
+//Create Rider Table
+app.get('/createRiderTable', (req, res) => {
+    let query = 'CREATE TABLE rider(id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255), current_location POINT)';
+    db.query(query, (err, result) => {
+        if(err) 
+            throw err;
+        console.log(result);
+        res.send('Rider Table Created');
+    })
+});
+
+//Create Driver Table
+app.get('/createDriverTable', (req, res) => {
+    let query = 'CREATE TABLE driver(id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255), car varchar(255), current_location POINT)';
+    db.query(query, (err, result) => {
+        if(err) 
+            throw err;
+        console.log(result);
+        res.send('Driver Table Created');
+    })
+});
+
+//Create  a Rider
+app.get('/create/rider', (req, res) => {
+    let rider = {
+        name: 'Jahid',
+        // current_location: POINT( req.body.current_locationX,req.body.current_locationY),
+        current_location: GeomFromText('POINT(18 23)'),
+    }
+
+    let query = 'INSERT INTO rider SET ?';
+    db.query(query, rider, (err, result) => {
+        if(err) 
+            throw err;
+        console.log(result);
+        res.send('Rider Created');
     })
 });
 
