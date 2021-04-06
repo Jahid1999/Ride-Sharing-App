8e1b28dd9eeb1e54e9f3119592dd8616b840fafb
Jahid1999 <bsse1030@iit.du.ac.bd>
2021-04-07 00:07:31 +0600

    rating post done

--- index.js
+++ index.js
@@ -37,46 +37,80 @@ app.get('/createDB', (req, res) => {
     })
 });
 
-//Create Rider Table
-app.get('/createRiderTable', (req, res) => {
-    let query = 'CREATE TABLE rider(id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255), current_location POINT)';
+// //Create Rider Table
+
+// app.get('/createRiderTable', (req, res) => {
+//     let query = 'CREATE TABLE rider(id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255), current_location POINT)';
+//     db.query(query, (err, result) => {
+//         if(err) 
+//             throw err;
+//         console.log(result);
+//         res.send('Rider Table Created');
+//     })
+// });
+
+// //Create Driver Table
+
+// app.get('/createDriverTable', (req, res) => {
+//     let query = 'CREATE TABLE driver(id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255), car varchar(255), current_location POINT)';
+//     db.query(query, (err, result) => {
+//         if(err) 
+//             throw err;
+//         console.log(result);
+//         res.send('Driver Table Created');
+//     })
+// });
+
+//Create Driver Rating Table
+
+app.get('/createDriverRatingTable', (req, res) => {
+    let query = 'CREATE TABLE ratings(id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255), car varchar(255), rating double)';
     db.query(query, (err, result) => {
         if(err) 
             throw err;
         console.log(result);
-        res.send('Rider Table Created');
+        res.send('Driver Rating Table Created');
     })
 });
 
-//Create Driver Table
-app.get('/createDriverTable', (req, res) => {
-    let query = 'CREATE TABLE driver(id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255), car varchar(255), current_location POINT)';
-    db.query(query, (err, result) => {
-        if(err) 
-            throw err;
-        console.log(result);
-        res.send('Driver Table Created');
-    })
-});
-
-//Create  a Rider
-app.get('/create/rider', (req, res) => {
-    let rider = {
-        name: 'Jahid',
-        // current_location: POINT( req.body.current_locationX,req.body.current_locationY),
-        current_location: GeomFromText('POINT(18 23)'),
+//Store Rating
+app.post('/ratings', (req, res) => {
+    let a = {
+        name: req.body.name,
+        car: 'B',
+        rating: 4.00,
     }
 
-    let query = 'INSERT INTO rider SET ?';
-    db.query(query, rider, (err, result) => {
+
+    // let query = "INSERT INTO ratings (name, car, rating) VALUES ( ?, ?, ?)";
+    let query = "INSERT INTO ratings SET ?";
+    db.query(query, a, (err, result) => {
         if(err) 
             throw err;
         console.log(result);
-        res.send('Rider Created');
+        res.send('Rating Created');
     })
+    res.send('Rating Created');
 });
 
-const PORT = process.env.PORT || 5000;
+// //Create  a Rider
+// app.get('/create/rider', (req, res) => {
+//     let rider = {
+//         name: 'Jahid',
+//         // current_location: POINT( req.body.current_locationX,req.body.current_locationY),
+//         current_location: GeomFromText('POINT(18 23)'),
+//     }
+
+//     let query = 'INSERT INTO rider SET ?';
+//     db.query(query, rider, (err, result) => {
+//         if(err) 
+//             throw err;
+//         console.log(result);
+//         res.send('Rider Created');
+//     })
+// });
+
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
     console.log(`Server Running on port: ${PORT}`);
