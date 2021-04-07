3d469a6ce3849eef5c1ed2c63c2d095cdce50aa7
Jahid1999 <bsse1030@iit.du.ac.bd>
2021-04-07 14:15:05 +0600

    socket io and scheduler added

---
+++ client.js
@@ -0,0 +1,31 @@
+const io = require('socket.io-client')
+const http = require('http')
+const riders = require('./Riders');
+
+let socket = io.connect('http://localhost:9001/communication')
+
+const search = {
+    id: '',
+    positionX: Math.random(),
+    positionY: Math.random(),
+}
+
+const fetchAllRiders = {
+    hostname: 'localhost',
+    port: 5000,
+    path: '/api/communication',
+    method: 'Get',
+}
+
+
+const reqRider = http.request(fetchAllRiders, res => {
+    console.log(`statusCode: ${res.statusCode} \n`)
+    console.log(res)
+    console.log("we are good to go")
+})
+reqRider.end()
+
+
+socket.on('welcome',(data)=>{
+    console.log(data)
+})
--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const { POINT_CONVERSION_COMPRESSED } = require('constants');
 const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
+const http = require('http').createServer()
+const sch = require('node-schedule')
+
+const io = require('socket.io')(http)
+
+io.of('communication').on('connection', (socket)=>{
+    console.log("new user connected")
+    const job = sch.scheduleJob('*/5 * * * * *', function(){
+        socket.emit("welcome","User is connected")
+    });
+})
 
 //Create Connection
 
@@ -35,7 +46,13 @@ app.use(express.urlencoded({ extended: false}));
 app.use('/api', require('./routes/root'));
 app.use('/api', require('./routes/driver'));
 
+
+const Sckt = 9001;
 const PORT = process.env.PORT || 5000;
+
+http.listen(Sckt,()=>{
+    console.log(`Socket Running on ${Sckt}`);
+})
 
 app.listen(PORT, () => {
     console.log(`Server Running on port: ${PORT}`);
